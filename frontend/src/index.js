ee26b8855d9226aa814cefc6ed288c23957cec33
shaziakaleem <shaziakaleem1616@gmail.com>
2024-09-18 20:20:57 +0530

    changed favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
-const a = false;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {a ? <App/> : <h1>hello from JSx</h1>}
+    <App/>
   </React.StrictMode>
 );
