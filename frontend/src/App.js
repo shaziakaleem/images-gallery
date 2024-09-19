41502417198fa4fb9bb69ff0fba521ae3780d0fd
shaziakaleem <shaziakaleem1616@gmail.com>
2024-09-19 18:28:49 +0530

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
   return (
     <div className="App">
-      <Header title = "Images Gallery"/>
+      <Header title="Images Gallery" />
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar,Container} from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor : "lightblue"
+};
 
 const Header = (props) => {
     const {title} = props
     return (
-        <Navbar bg="light" data-bs-theme="dark">
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Navbar style={navbarStyle} data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand> 
+            </Container>
       </Navbar>
     )
 };
